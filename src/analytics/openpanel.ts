eeca9d071fa3a544f9366285af2cba7cf4300268
Julien Calixte <julien.calixte@theodo.com>
2026-05-17 00:17:31 +0200

    chore(analytics): point openpanel to stats apoena endpoint

--- src/analytics/openpanel.ts
+++ src/analytics/openpanel.ts
@@ -1,7 +1,7 @@
 import { OpenPanel } from "@openpanel/web"
 
 export const op = new OpenPanel({
-  apiUrl: "https://api.panel.apoena.dev",
+  apiUrl: "https://api.stats.apoena.dev",
   clientId: "038a6aac-19bb-4a7f-9aae-2d0201fead5b",
   trackScreenViews: true,
   trackOutgoingLinks: true,
